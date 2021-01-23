7172ae41e415eae91bbc9b9a4ff4d641ea16cf63
jung <jung@corona-bytes.net>
2021-01-23 23:23:30 +0100

    -

--- StimulsoftClient/src/app/app.component.ts
+++ StimulsoftClient/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
+import {BehaviorSubject} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'stimulsoft';
+export class AppComponent implements AfterViewInit{
+  angular = false;
+  razor = false;
+  clientHeight = '80vh';
+
+  @ViewChild('host')
+  host: ElementRef | null = null;
+
+  constructor() { }
+
+  updateHeight(height: any): void {
+    if (height !== 0 && height !== undefined) {
+      if (this.clientHeight !== height + 'px') {
+        this.clientHeight = height + 'px';
+      }
+    }
+  }
+
+  ngAfterViewInit(): void {
+    if (this.host) {
+      this.updateHeight(this.host.nativeElement.clientHeight);
+    }
+  }
 }
